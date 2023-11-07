12deb6217d05846a391169c7c66e2717fd915785
CM Antony <81664460+CMA020@users.noreply.github.com>
2023-11-07 16:21:45 +0530

    sa

--- UI.py
+++ UI.py
@@ -19,9 +19,16 @@ def pred(img):
     # Apply AHE to the image
     equalized_image = clahe.apply(image)
     bgr_image = cv2.cvtColor(equalized_image, cv2.COLOR_GRAY2BGR)
+<<<<<<< Updated upstream
 
     cv2.waitKey(1)
     results = model(bgr_image,imgsz=2048)
+=======
+    img = cv2.resize(img, (2048, 2048))
+    bgr_image = cv2.resize(bgr_image, (2048, 2048))
+    results = model(bgr_image,imgsz=(2048,2048))
+    #results = model(resized_image)
+>>>>>>> Stashed changes
     for result in results:
         boxes = result.boxes  # Boxes object for bbox outputs
         masks = result.masks  # Masks object for segmentation masks outputs
@@ -36,16 +43,32 @@ def pred(img):
                 w = int(box.xywh[0][2])
                 h = int(box.xywh[0][3])
                 i = int(box.cls)
+<<<<<<< Updated upstream
                 cv2.rectangle(img, (x-int(w/2), y-int(h/2)), (x+int(w/2), y+int(h/2)), (0, 0, 255), thickness=4)
     return img
 
+=======
+                cv2.rectangle(img, (x-int(w/2), y-int(h/2)), ((x + int(w/2)), (y + int(h/2))), (0, 0, 255), thickness=6)
+                print(x)
+                print(y)
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     image = cv2.imread("15.jpg")
     final=pred(image)
     resized_image = cv2.resize(image, (900,900))
     cv2.imshow("win",resized_image)
+=======
+    image = cv2.imread('15.jpg')
+
+    final=predict(image)
+    resized_image = cv2.resize(image, (900,900))
+    cv2.imshow("out", final)
+    #cv2.imwrite("out.jpg",final)
+    #cv2.imshow("out", resized_image)
+>>>>>>> Stashed changes
     cv2.waitKey(0)
 
 
