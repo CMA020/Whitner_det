8e7e5975f337e8f37075a9cbf31a0257ab65ad2f
CM Antony <81664460+CMA020@users.noreply.github.com>
2023-11-08 12:04:08 +0530

    er

--- Det_test.py
+++ Det_test.py
@@ -2,14 +2,14 @@ from ultralytics import YOLO
 import os
 import cv2
 
-model = YOLO(os.path.expanduser('~/Whitner_det/best_W_3.pt'))
+model = YOLO(os.path.expanduser('~/Whitner_det/last_48_9_W.pt'))
 clip_limit = 78  # Set your desired clip limit (78 in this example)
 tile_size = 20
 
 
 if __name__ == '__main__':
     while True:
-        image = cv2.imread("13.jpg", cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread("12.jpg", cv2.IMREAD_GRAYSCALE)
 
         # Create an instance of the CLAHE (Contrast Limited Adaptive Histogram Equalization) class
         clahe = cv2.createCLAHE(clipLimit=clip_limit / 10.0, tileGridSize=(tile_size, tile_size))
